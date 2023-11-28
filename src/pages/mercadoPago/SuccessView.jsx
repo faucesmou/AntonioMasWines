47071c31136c7446f0d848da57c9a195176d9d89
devopsandes <gonzalomorresi@gmail.com>
2023-11-28 15:15:23 -0300

    modificacion de successView para mejorar vista

--- src/pages/mercadoPago/SuccessView.jsx
+++ src/pages/mercadoPago/SuccessView.jsx
@@ -4,15 +4,13 @@ import React from 'react';
 const SuccessView = () => {
 
     const externalReference = window.location.pathname.split('/').pop();
-    console.log('este es el external Reference: ', externalReference )
+    console.log('este es el external Reference: ', externalReference );
     
   return (
     <div>
       <h1>Pago Exitoso</h1>
       <p>¡Gracias por tu compra!</p>
       <p>Referencia Externa: {externalReference}</p>
-      <h2>Referencia Externa: {externalReference}</h2>
-      <h2> ´Referencia Externa: ${externalReference}´</h2>
     </div>
   );
 };

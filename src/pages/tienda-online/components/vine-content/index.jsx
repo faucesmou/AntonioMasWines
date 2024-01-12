5772f8a1f47550113ce07655d60043618e0e30dc
devopsandes <gonzalomorresi@gmail.com>
2024-01-12 11:08:04 -0300

    Modificaciones y funciones para correcta visualización utilizando ruta desde la base de datos.

--- src/pages/tienda-online/components/vine-content/index.jsx
+++ src/pages/tienda-online/components/vine-content/index.jsx
@@ -3,9 +3,12 @@ import S1 from "../../../../assets/imgs/s1.png";
 import P1 from "../../../../assets/imgs/p1.png";
 import P2 from "../../../../assets/imgs/p2.png";
 import P3 from "../../../../assets/imgs/p3.png";
+
 import A1 from "../../../../assets/imgs/amarada1002.png";
 /* import A2 from "../../../../assets/imgs/almaradaCavernett.png"; */
 import A2 from "../../../../assets/imgs/AlmaradaCavernetSinFondo.png";
+
+
 import { FiShoppingCart } from "react-icons/fi";
 import { BsArrowUpCircle, BsFileEarmarkArrowDown } from "react-icons/bs";
 import Rating from "./rating";
@@ -36,14 +39,14 @@ const wineCardData = [
         image: P1,
         text: "Single Vineyard",
         subText: "Cabernet (x6)",
-        price: "Sin Stock",
+        price: 100,
         btnText: "Añadir al carrito",
     },
     {
         image: P2,
         text: "Single Vineyard",
         subText: "Chardonay (x6)",
-        price: "Sin Stock",
+        price: 200,
         btnText: "Añadir al carrito",
     },
     /*  {
@@ -57,21 +60,21 @@ const wineCardData = [
         image: S1,
         text: "Single Vineyard",
         subText: "Malbec (x6)",
-        price: "Sin Stock",
+        price: 300,
         btnText: "Añadir al carrito",
     },
     {
         image: P1,
         text: "Single Vineyard",
         subText: "Cabernet (x6)",
-        price: "Sin Stock",
+        price: 10000,
         btnText: "Añadir al carrito",
     },
     {
         image: P2,
         text: "Single Vineyard",
         subText: "Chardonay (x6)",
-        price: "Sin Stock",
+        price: 50000,
         btnText: "Añadir al carrito",
     },
     /* {
@@ -85,7 +88,7 @@ const wineCardData = [
         image: A1,
         text: "Almarada",
         subText: "Malbec (x6)",
-        price: "Sin Stock",
+        price: 70000,
         btnText: "Añadir al carrito",
     },
 ];
@@ -155,6 +158,7 @@ const VineContent = () => {
 
     const [ProductosFiltrados, setProductosFiltrados] = useState([]);
     const [filterType, setFilterType] = useState("menor precio");
+    const [imagenesCargadas, setImagenesCargadas] = useState([]);
   /*   const [recargaMayorPrecio, setrecargaMayorPrecio] = useState(""); */
   /*   const [filterType, setFilterType] = useState("menor precio"); */
 
@@ -165,8 +169,13 @@ const VineContent = () => {
         console.log('Este es el wineCardData5: -->', wineCardData5);
 
         /*  let filteredProducts = [...wineCardData]; */
+        /* emparejo la estructura de wineCardData5 a la que necesito para que sea leida correctamente: */
 
-        let filteredProducts = wineCardData;
+        wineCardData5.forEach(product => {
+            product.price = parseFloat(product.price.replace('$', ''));
+          });
+
+        let filteredProducts = wineCardData5;
         console.log('estos son los filteredProducts: -->', filteredProducts);
         let pruebaDato = filteredProducts[0].price;
         typeof (pruebaDato)
@@ -189,8 +198,27 @@ const VineContent = () => {
     };
 
     useEffect(() => {
+        console.log('Ejecutando useEffect con filterType:', filterType);
         filterProducts()
     }, [filterType] )
+
+    // Función para cargar las imágenes de manera asíncrona
+    const cargarImagenes = async () => {
+        const imagenes = await Promise.all(
+            ProductosFiltrados.map(async ({ image }) => {
+                const rutaCompleta = `${image.replace(/'/g, '"')}`;
+                console.log('Ruta completa:', rutaCompleta);
+
+                const imagen = await import(rutaCompleta).then((module) => module.default);
+                return imagen;
+            })
+            );
+            setImagenesCargadas(imagenes);
+        };
+        
+        useEffect(() => {
+        cargarImagenes();
+      }, [ProductosFiltrados]);
     
     
 
@@ -331,22 +359,26 @@ const VineContent = () => {
                                 {ProductosFiltrados.map(
                                     ({ image, text, subText, price, btnText, formattedPrice }, i) => (
                                         <CustomWineCard
-                                            image={image}
-                                            /*   minW="150px"
-                                              minH="370.53px" */
-                                            key={i}
-                                            /* onAddToCart={() => addToCart(image, text, price)} */
-                                            text={text}
-                                            subText={subText}
-                                            /*  price={price} */
-                                            price={/* formattedPrice */ price.toLocaleString('es-AR')} //uso el precio formateado
-                                            btnText={btnText}
-                                            style={{ color: "black" }}
+                                        
+                                        /* image={image}  */
+                                        /*  image={import(`../../${image}`).default}  */// Importa la imagen utilizando ES6 
+                                        /*   minW="150px"
+                                        minH="370.53px" */
+                                        
+                                        image={imagenesCargadas[i]}
+                                        key={i}
+                                        text={text}
+                                        subText={subText}
+                                        price={/* formattedPrice */ price.toLocaleString('es-AR')} //uso el precio formateado
+                                        btnText={btnText}
+                                        style={{ color: "black" }}
                                         />
-
-                                    )
-                                )}
+                                        
+                                        
+                                        )
+                                        )}
                             </SimpleGrid>
+                                       
                         </Box>
                     </Flex>
 

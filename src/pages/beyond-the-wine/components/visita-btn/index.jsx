ec526dafd5c580895de101aec6b361530532fb91
devopsandes <gonzalomorresi@gmail.com>
2024-02-19 16:56:01 -0300

    Proyecto Antonio Más Wines

--- src/pages/beyond-the-wine/components/visita-btn/index.jsx
+++ src/pages/beyond-the-wine/components/visita-btn/index.jsx
@@ -1,30 +1,52 @@
-import { Box, Avatar, Link, Flex } from "@chakra-ui/react";
+import { Box, Avatar, Link, Flex, useBreakpointValue } from "@chakra-ui/react";
 import CustomButton from "../../../../components/button";
 import { AiFillYoutube } from "react-icons/ai";
 import { BsYoutube } from "react-icons/bs";
 
 const VisitaBtn = () => {
-  const youtubeURL = "https://www.youtube.com";
+
+  const youtubeURL = "https://www.youtube.com/watch?v=MYcGs4PRGu8&ab_channel=WineTalkswithPaulK";
+  
+  const youtubeButtonTextSize = useBreakpointValue({
+    base: "12px", // Tamaño de fuente para tamaños base y más pequeños
+    md: "15px", // Tamaño de fuente para tamaños md y más grandes
+    flexDirection:"column",
+    justifyContent: "center"
+  });
+
+  const textAlignment = useBreakpointValue({
+    base: "center", // Centrado para tamaños base
+    md: "center", // Centrado para tamaños md y más grandes
+  });
+  const margenConImagen = useBreakpointValue({
+    base: 1, // Centrado para tamaños base
+    md: 4, // Centrado para tamaños md y más grandes
+  });
+
+
   return (
-    <Box px={10} bg="bgDark" py={10}>
+    <Box px={10} bg="bgDark" py={10} >
      <Link href={youtubeURL} isExternal>
         <Flex
           bg="rgba(0, 0, 0, 0.5)"
           border="1px solid rgba(255,255,255,.5)"
-          w={{ base: "fit-content", md: "100%" }}
+          w={{ base: "fit-content", md: "40%" /*,  md: "center", */ }}
+          mx="auto"
           py={{ base: 9, md: 3 }}
-          p={3}
+          p={2}
           borderRadius="50px"
           color="white"
          /*  _hover={{ bg: "black", transform: "scale(1.1)" }} */
           alignItems="center" 
           justifyContent={"center"}
+          fontSize={youtubeButtonTextSize} // Aplicar el tamaño de fuente condicionalmente
+          textAlign={textAlignment}
         >
-          <span>Visitá nuestro canal de YouTube</span>
+          <span  >Visitá nuestro canal de YouTube</span>
           <Avatar
             bg="bgLight"
             icon={<BsYoutube fontSize="1.5rem" color="black" />}
-            ml={2} // Margen a la izquierda para separar el icono del texto
+            ml={margenConImagen} // Margen a la izquierda para separar el icono del texto
           />
         </Flex>
       </Link>
